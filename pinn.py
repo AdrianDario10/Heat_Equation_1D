ab6b52675821e915ec1cda403d3f207ec49389bf
Adrián Darío Miguel Quintas <99609206+AdrianDario10@users.noreply.github.com>
2022-05-31 18:35:04 +0200

    Create pinn.py

---
+++ pinn.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from .layer import GradientLayer
+
+class PINN:
+    """
+    Build a physics informed neural network (PINN) model for the heat equation.
+    Attributes:
+        network: keras network model with input (t, x) and output u(t, x).
+        c: 2
+        grads: gradient layer.
+    """
+
+    def __init__(self, network, c=2):
+        """
+        Args:
+            network: keras network model with input (t, x) and output u(t, x).
+            c: wave velocity. Default is 1.
+        """
+
+        self.network = network
+        self.c = c
+        self.grads = GradientLayer(self.network)
+
+    def build(self):
+        """
+        Build a PINN model for the wave equation.
+        Returns:
+            PINN model for the projectile motion with
+                input: [ (t, x) relative to equation,
+                         (t=0, x) relative to initial condition,
+                         (t, x=bounds) relative to boundary condition ],
+                output: [ u(t,x) relative to equation,
+                          u(t=0, x) relative to initial condition,
+                          du_dt(t=0, x) relative to initial derivative of t,
+                          u(t, x=bounds) relative to boundary condition ]
+        """
+
+        # equation input: (t, x)
+        tx_eqn = tf.keras.layers.Input(shape=(2,))
+        # initial condition input: (t=0, x)
+        tx_ini = tf.keras.layers.Input(shape=(2,))
+        # boundary condition input: (t, x=-1) or (t, x=+1)
+        ##tx_bnd_up = tf.keras.layers.Input(shape=(2,))
+        ##tx_bnd_down = tf.keras.layers.Input(shape=(2,))
+        tx_bnd = tf.keras.layers.Input(shape=(2,))
+        tx_bnd_up = tf.keras.layers.Input(shape=(2,))
+        tx_bnd_down = tf.keras.layers.Input(shape=(2,))
+
+        # compute gradients
+        _, du_dt, _, d2u_dt2, d2u_dx2 = self.grads(tx_eqn)
+
+        # equation output being zero
+        u_eqn = du_dt - self.c*self.c * d2u_dx2
+        # initial condition output
+        u_ini, du_dt_ini, _, _, _ = self.grads(tx_ini)
+        # boundary condition output
+        u_bnd_down, _, _, _, _ = self.grads(tx_bnd_down)
+        u_bnd_up, _, _, _, _ = self.grads(tx_bnd_up)
+        #u_bnd, _, _, _, _ = self.grads(tx_bnd)
+
+
+        ##u_bnd = self.network(tx_bnd_up) - self.network(tx_bnd_down)       #self.network(tx_bnd_up)         #PBC # dirichlet
+
+        #_, _,du_dx_up, _, _ = self.grads(tx_bnd_up)
+        #_, _,du_dx_down, _, _ = self.grads(tx_bnd_down)
+        #u_bnd_der =  du_dx_up  - du_dx_down                  #self.network(tx_bnd_down)
+        #_, _, u_bnd, _, _ = self.grads(tx_bnd)  # neumann
+
+        # build the PINN model for the wave equation
+        return tf.keras.models.Model(
+            inputs=[tx_eqn, tx_ini, tx_bnd_up,tx_bnd_down],
+            outputs=[u_eqn, u_ini, u_bnd_up,u_bnd_down])
